24761514c034f3d5a1bf736cb3076bc00305e2e8
Thomas Walsh <lwalsh7@luc.edu>
2020-10-30 00:37:45 -0500

    getting things set up in a git repo

--- COMP363A3/WALSH_3.1.py
+++ COMP363A3/WALSH_3.1.py
@@ -1,7 +1,6 @@
 def insertionSort(nums):
     for num in range(1, len(nums)):
         item = nums[num]
-        #Moves bigger guys ahead of smaller guys
         previous = num - 1
         while previous >= 0 and item < nums[previous]:
             nums[previous + 1] = nums[previous]
--- COMP363A3/WALSH_3.3.py
+++ COMP363A3/WALSH_3.3.py
@@ -1,11 +1,9 @@
 def selectionSort(nums):
     for num in range(len(nums)):
-        #finds smallest guy
         minimum = num
         for numnum in range(num+1, len(nums)):
             if nums[minimum] > nums[numnum]:
                 minimum = numnum
-        #switches little guy with first guy
         temp = nums[num]
         nums[num] = nums[minimum]
         nums[minimum] = temp
